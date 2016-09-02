3cae7330597a7be19996c3d354599c09aa9f92a7
inpyokim <inpyokim@gmail.com>
2016-09-02 14:06:47 +0900

    comment bug fixed by inpyokim

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-// For command line git tutorial
-// First : Local repository modification
-// Second : GitHub modification
+# For command line git tutorial
+# First : Local repository modification
+# Second : GitHub modification
 print("Hello world")
 print("Tell Your World")
 print("Tell his world")
